541af966091f65c5f425f3b4eaff539d9e9b898b
Jeriming <316906528@qq.com>
2022-12-28 20:59:20 +0800

    组件&Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Lesson01 from './lesson/lesson01/Lesson01'
+import ClassComponent from './lesson/lesson01/ClassComponent'
+import FunctionComponent from './lesson/lesson01/FunctionComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello
+        <Lesson01></Lesson01>
+        <ClassComponent name="This is name"></ClassComponent>
+        <FunctionComponent name="this is FunctionComponent"></FunctionComponent>
       </header>
     </div>
   );
---
+++ src/lesson/lesson01/Lesson01.js
@@ -0,0 +1,6 @@
+function Lesson01() {
+  const name = "Josh Perez";
+  return <h1>Hello, {name}</h1>;
+}
+
+export default Lesson01;
